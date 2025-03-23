313a7d30a15f79259f26935aff057ff6ad240541
pangguanzheKM <1312999041@qq.com>
2025-03-23 17:32:00 +0800

    准备资源加载

---
+++ Assets/Scripts/Tools/UI.cs
@@ -0,0 +1,62 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public Transform allUI;
+    public CanvasGroup bg;
+
+    public virtual void ShowUI()
+    {
+        UIAni();
+    }
+
+    public virtual void ShowUI(bool isWin)
+    {
+        UIAni();
+    }
+
+    public virtual void ShowUI(Action YesCall, Action NoCall)
+    {
+        UIAni();
+    }
+
+    public virtual void ShowUI(string msg)
+    {
+        UIAni();
+    }
+
+    private void UIAni()
+    {
+        gameObject.SetActive(true);
+        bg.alpha = 0;
+        allUI.localScale = Vector3.zero;
+        bg.DOFade(1, 0.2f);
+        allUI.DOScale(1.1f, 0.2f).OnComplete(() =>
+        {
+            allUI.DOScale(1, 0.1f);
+        });
+    }
+
+    public virtual void HideUI()
+    {
+        gameObject.SetActive(false);
+
+    }
+
+    public virtual void CloseMe()
+    {
+        bg.alpha = 1;
+        bg.DOFade(0, 0.2f);
+        allUI.DOScale(0, 0.2f).OnComplete(() =>
+        {
+            HideUI();
+        });
+
+    }
+
+  
+}

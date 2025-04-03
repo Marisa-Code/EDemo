a85c1af42b876a9bead490902ce83059da6f7448
pangguanzheKM <1312999041@qq.com>
2025-04-03 14:41:28 +0800

    测试中

--- Assets/GameMain/Scripts/Editor/GameFrameworkConfigs.cs
+++ Assets/GameMain/Scripts/Editor/GameFrameworkConfigs.cs
@@ -1,15 +1,30 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using GameFramework;
-using UnityGameFramework.Editor.ResourceTools;
 using System.IO;
-namespace EdgeShimmer
+using UnityEngine;
+using UnityGameFramework.Editor;
+using UnityGameFramework.Editor.ResourceTools;
+
+namespace EdgeShimmer.Editor
 {
     public static class GameFrameworkConfigs
     {
+        [BuildSettingsConfigPath]
+        public static string BuildSettingsConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/BuildSettings.xml"));
+
+        [ResourceCollectionConfigPath]
+        public static string ResourceCollectionConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceCollection.xml"));
+
         [ResourceEditorConfigPath]
         public static string ResourceEditorConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceEditor.xml"));
+
+        [ResourceBuilderConfigPath]
+        public static string ResourceBuilderConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceBuilder.xml"));
     }
 }

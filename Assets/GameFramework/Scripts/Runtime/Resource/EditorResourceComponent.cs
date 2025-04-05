59fb4178c94e9908b2e8bcc48abf3684adaae210
pangguanzheKM <1312999041@qq.com>
2025-04-05 11:43:15 +0800

    导入开源代码

--- Assets/GameFramework/Scripts/Runtime/Resource/EditorResourceComponent.cs
+++ Assets/GameFramework/Scripts/Runtime/Resource/EditorResourceComponent.cs
@@ -490,6 +490,26 @@ namespace UnityGameFramework.Runtime
 #pragma warning disable 0067, 0414
 
         /// <summary>
+        /// 资源校验开始事件。
+        /// </summary>
+        public event EventHandler<GameFramework.Resource.ResourceVerifyStartEventArgs> ResourceVerifyStart = null;
+
+        /// <summary>
+        /// 资源校验成功事件。
+        /// </summary>
+        public event EventHandler<GameFramework.Resource.ResourceVerifySuccessEventArgs> ResourceVerifySuccess = null;
+
+        /// <summary>
+        /// 资源校验失败事件。
+        /// </summary>
+        public event EventHandler<GameFramework.Resource.ResourceVerifyFailureEventArgs> ResourceVerifyFailure = null;
+
+        /// <summary>
+        /// 资源应用开始事件。
+        /// </summary>
+        public event EventHandler<GameFramework.Resource.ResourceApplyStartEventArgs> ResourceApplyStart = null;
+
+        /// <summary>
         /// 资源应用成功事件。
         /// </summary>
         public event EventHandler<GameFramework.Resource.ResourceApplySuccessEventArgs> ResourceApplySuccess = null;
@@ -824,6 +844,11 @@ namespace UnityGameFramework.Runtime
         public void UpdateVersionList(int versionListLength, int versionListHashCode, int versionListCompressedLength, int versionListCompressedHashCode, UpdateVersionListCallbacks updateVersionListCallbacks)
         {
             throw new NotSupportedException("UpdateVersionList");
+        }
+
+        public void VerifyResources(int verifyResourceLengthPerFrame, VerifyResourcesCompleteCallback verifyResourcesCompleteCallback)
+        {
+            throw new NotImplementedException("VerifyResources");
         }
 
         /// <summary>
